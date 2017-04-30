868d16ab58afd3d2303c50e80eefe409a5c52e33
Matthew Coppola <matt@yellowdig.com>
2017-04-29 20:02:06 -0400

    refactored lists page a bit to do fetching in componentDidMount

--- pages/lists.js
+++ pages/lists.js
@@ -14,51 +14,43 @@ import { getAlbums } from '../utils/lastfm'
 
 
 export default class extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { albums: [] }
-  }
-
-  static async getInitialProps (ctx) {
+  static getInitialProps (ctx) {
     const userToken = process.browser ? getTokenFromLocalStorage() : getTokenFromCookie(ctx.req)
-
-    let id = ctx.query.id || ''
-
-    let api = apiRequest(userToken)
-    let list = await api.get('/lists/'  + id )
-
-    return { list: list.data, userToken }
+    return { userToken, listId: ctx.query.id }
   }
 
-
-  componentDidMount() {
-    console.log(this.props.list);
+  async componentDidMount() {
+    let id = this.props.listId
+    let api = apiRequest(this.props.userToken)
+    let list = await api.get('/lists/'  + id )
+    this.setState({ list: list.data })
   }
 
   render() {
+    let l = this.state ? this.state.list : null;
+
     return (
       <div>
         <Head>
           <title>Mew</title>
-          <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css"/>
+          <link rel="stylesheet" href="/static/tachyons.min.css"/>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <div>
           <div {...styles.inner} className="cf mw7 mt5">
             <Header userToken={ this.props.userToken } />
-            <h2 {...styles.title} className="f4 lh-title ttu">{this.props.list._user.username} / {this.props.list.title}</h2>
+            <h2 {...styles.title} className="f4 lh-title ttu">{l && l._user.username} / {l && l.title}</h2>
             <div {...styles.chart}>
-            {this.props.list._albums.map((album, i) => {
+            {l && l._albums.map((album, i) => {
               return (
-                    <div className="cf mb2">
-                      <div className="fl w-10">
-                        <img width="50" height="50" src={album.image[1]} alt=""/>
-                      </div>
-                      <div className="fl w-90">
-                        <div className="f6 mv3">{i + 1}. &nbsp; {album.artist} - {album.title}</div> 
-                      </div>
+                  <div className="cf mb2">
+                    <div className="fl w-10">
+                      <img width="50" height="50" src={album.image[1]} alt=""/>
                     </div>
+                    <div className="fl w-90">
+                      <div className="f6 mv3">{i + 1}. &nbsp; {album.artist} - {album.title}</div> 
+                    </div>
+                  </div>
                 );
             })}
             </div>
